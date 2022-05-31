c6a50642f19748d48bb0177e2a5febd96c5b37c1
Jean Carlos Reyes Encarnacion <yacalo45@hotmail.es>
2022-05-30 23:07:08 -0400

    Bootstrap UPDATE

--- PokeDex/Startup.cs
+++ PokeDex/Startup.cs
@@ -17,7 +17,7 @@ namespace PokeDex
         {
             Configuration = configuration;
         }
-
+        
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
