f8e8372cedc2f11dcfe26b5dd4383cf006217a85
Jean Carlos Reyes Encarnacion <yacalo45@hotmail.es>
2022-06-02 23:43:52 -0400

    Type Repository Working

--- Pokedex.Application/Repository/TypeRepository.cs
+++ Pokedex.Application/Repository/TypeRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Pokedex.Database;
 using System;
 using System.Collections.Generic;
@@ -20,6 +21,42 @@ namespace Pokedex.Application.Repository
         public SelectList TypeDropDown() {
 
             return new SelectList(_dbContext.Set<Database.Models.Type>().ToList(), "Id", "Name");
+        }
+
+        public async Task AddTypeAsync(Database.Models.Type type) {
+
+            await _dbContext.Types.AddAsync(type);
+
+            await _dbContext.SaveChangesAsync();
+
+        }
+
+        public async Task<List<Database.Models.Type>> GetTypesAsync(){
+
+            return await _dbContext.Set<Database.Models.Type>().ToListAsync();
+
+        }
+
+        public async Task<Database.Models.Type> GetTypeByIdAsync(int Id) {
+
+            return await _dbContext.Set<Database.Models.Type>().FindAsync(Id);
+
+        }
+
+        public async Task UpdateTypeAsync(Database.Models.Type type) {
+
+            _dbContext.Entry(type).State = EntityState.Modified;
+
+            await _dbContext.SaveChangesAsync();
+        
+        }
+
+        public async Task DeleteTypeAsync(Database.Models.Type type) {
+
+            _dbContext.Set<Database.Models.Type>().Remove(type);
+
+            await _dbContext.SaveChangesAsync();
+
         }
     }
 }
---
+++ Pokedex.Application/ViewModel/AddTypeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex.Application.ViewModel
+{
+    public class AddTypeViewModel
+    {
+        public int TypeId { get; set; }
+        public string Name { get; set; }
+    }
+}
