e4142f4a03c2e6b3929766b9f50abf5805f43b37
Jean Carlos Reyes Encarnacion <yacalo45@hotmail.es>
2022-06-02 23:29:02 -0400

    Region Services Working

--- Pokedex.Application/Services/RegionServices.cs
+++ Pokedex.Application/Services/RegionServices.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using Pokedex.Application.Repository;
+using Pokedex.Application.ViewModel;
 using Pokedex.Database;
+using Pokedex.Database.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,5 +23,53 @@ namespace Pokedex.Application.Services
 
             return _repository.RegionDropDown();
         }
+
+        public async Task AddRegion(RegionViewModel vm) {
+
+            Region region = new() {
+            Id = vm.RegionId,
+            Name = vm.Name
+            };
+
+            await _repository.AddRegionAsync(region);
+        }
+
+        public async Task<List<RegionViewModel>> GetRegions()
+        {
+
+            var regionList = await _repository.GetRegionsAsync();
+
+            return regionList
+                .Select(region => new RegionViewModel { RegionId = region.Id, Name = region.Name })
+                .ToList();
+        }
+
+        public async Task<RegionViewModel> GetRegionById(int id) {
+
+            var region = await _repository.GetRegionsById(id);
+
+            return new() { RegionId = region.Id, Name = region.Name };
+        }
+
+        public async Task UpdateRegion(RegionViewModel vm) {
+
+            Region region = new()
+            {
+                Id = vm.RegionId,
+                Name = vm.Name
+            };
+
+            await _repository.UpdateRegionAsync(region);
+
+        }
+
+        public async Task DeleteRegion(int id) {
+
+            var region = await _repository.GetRegionsById(id);
+
+            await _repository.DeleteRegionAsync(region);
+        }
+
+
     }
 }
---
+++ Pokedex.Application/ViewModel/AddRegionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex.Application.ViewModel
+{
+    public class AddRegionViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage="Name is required. Ex. Kanto")]
+        public string Name { get; set; }
+    }
+}
