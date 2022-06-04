57a1501bf16b1a5648f4b7ee645ca9a99cf47929
Jean Carlos Reyes Encarnacion <yacalo45@hotmail.es>
2022-06-04 16:17:21 -0400

    TypeView Working Fine.

---
+++ PokeDex/Controllers/TypeController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pokedex.Application.Services;
+using Pokedex.Application.ViewModel;
+using Pokedex.Database;
+using System.Threading.Tasks;
+
+namespace PokeDex.Controllers
+{
+    public class TypeController : Controller
+    {
+        private readonly TypeServices _services;
+
+        public TypeController(ApplicationContext dbContext)
+        {
+            _services = new(dbContext);
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            return View(await _services.GetTypes());
+        }
+
+        public IActionResult Create()
+        {
+            return View("CreateType", new AddTypeViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AddTypeViewModel vm) {
+
+            if (!ModelState.IsValid)
+            {
+                return View("CreateType",vm);
+            }
+
+            await _services.AddType(vm);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int Id) {
+
+            return View("CreateType", await _services.GetTypeById(Id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(AddTypeViewModel vm) {
+
+            if (!ModelState.IsValid)
+            {
+                return View("CreateType",vm);
+            }
+
+            await _services.UpdateType(vm);
+
+            return RedirectToAction("Index");
+
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+
+            return View(await _services.GetTypeById(Id));
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeletePost(int TypeId) {
+
+            await _services.DeleteType(TypeId);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Pokedex.Application/ViewModel/AddTypeViewModel.cs
+++ Pokedex.Application/ViewModel/AddTypeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Pokedex.Application.ViewModel
     public class AddTypeViewModel
     {
         public int TypeId { get; set; }
+        [Required(ErrorMessage = "The name is required Ex. Water")]
         public string Name { get; set; }
     }
 }
